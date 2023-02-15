7edd1e9974934452bd619cd4b8cd86e695641b19
Abdusamad <abdusattorov.coder@gmail.com>
2023-02-16 00:08:58 +0500

    0989_add_to_array-form_of_integer.py | lazy | fix

--- python/0989_add_to_array-form_of_integer.py
+++ python/0989_add_to_array-form_of_integer.py
@@ -1,14 +1,14 @@
 class Solution:
     def addToArrayForm(self, num, k):
-        temp = 0
+        temp = ""
         for i in num:
-            temp += i
-        temp += k
-        temp = str(temp)
-        return list(temp)
+            temp += str(i)
+        temp = int(temp) + k
+        res = list(map(int, str(temp)))
+        return res
 
 
 if __name__ == '__main__':
     test = Solution()
-    print(test.addToArrayForm(num = [1,2,0,0], k = 34))
-    print(test.addToArrayForm(num = [2,7,4], k = 181))
+    print(test.addToArrayForm(num = [1,2,0,0], k = 34)) # [1,2,3,4]
+    print(test.addToArrayForm(num = [2,7,4], k = 181)) # [4,5,5]
